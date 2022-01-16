13b2913b8b93b71c3c54c89634e0a9420ebcc0be
salima5214 <mingming5214@gmail.com>
2022-01-16 16:58:17 +0800

    add button 'Load More' to show more pictures

--- week-3/util_modification.js
+++ week-3/util_modification.js
@@ -1,34 +1,53 @@
 
+let row = 1;
+
 function showPicture(){
     let main = document.querySelector(".picture");
 
-    for(let row = 1; row < 3; row++){
-        for(let i = 4*(row-1); i < 4*row; i++){
+    for(let i = 0; i < 2; i++){
+        for(let j = 4*(row-1); j < 4*row; j++){
+            
+            if( j >= sight_information["result"]["results"].length ){
+                let button = document.querySelector(".load_more>button");
+                button.style.display = "none";
+            }
+
             let box = document.createElement("div");
             box.className = "box";
             let img = document.createElement("img");
-            img.src = "https://"+sight_information["result"]["results"][i]["file"].split("https://")[1];
+            img.src = "https://"+sight_information["result"]["results"][j]["file"].split("https://")[1];
             box.appendChild(img);      
             
             let img_name = document.createElement("div");
             img_name.className = "img_name";
-            img_name.textContent = sight_information["result"]["results"][i]["stitle"];
+            img_name.textContent = sight_information["result"]["results"][j]["stitle"];
             box.appendChild(img_name);
             main.appendChild(box);
         }
         let mybr = document.createElement("div");
         mybr.className = "line_break";
         main.appendChild(mybr);
+        row += 1;
     }
 
 
 }
+
 // **********************************************
 
+
+function loadMore() {
+    showPicture();
+}
+
+// **********************************************
+// len(data["result"]["results"])
 // **********************************************
 
 let url = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json";
 let sight_information; // global variable
+
+
 
 fetch(url).then((response) => {
     return response.json();
@@ -36,3 +55,4 @@ fetch(url).then((response) => {
     sight_information = url_data;
     showPicture();
 });
+
